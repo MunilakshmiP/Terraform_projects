405bc2c6b3b93980e8d218ab245c8a9ef1daabe0
Munilakshmi G J <110093082+MunilakshmiP@users.noreply.github.com>
2025-01-13 22:36:15 +0530

    Create variables.tf

---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_value" {
+  description = "Value of AMI"
+  default     = "ami-053b12d3152c0cc71"
+}
+
+variable "aws_instance_type" {
+  description = "Value of instance type"
+  default     = "t2.micro"
+}
